7039e423a5c8865f404aec4985f0a9a8ae0da158
XIhe04 <143445793+XIhe04@users.noreply.github.com>
2023-08-28 16:36:25 -0400

    Create class-00-dennett-summary.m
    Wrote my initial summary for the Dennett article

---
+++ contrib/Xihe/class-00-dennett-summary.m
@@ -0,0 +1,2 @@
+here is my initial summay for Dennett article.
+This article is talking about the negative effects that counterfeit people bring to us, such as destroying the economy, freedom of human, culture and so on. The counterfeit people will be very dangerous, because they may lose control, due to automatic reproduction. 
