a176fb99882bebfb465c99c4cfebcbd713fecfa9
XIhe04 <143445793+XIhe04@users.noreply.github.com>
2023-09-04 10:45:59 -0400

    Update class-00-dennett-summary.m

--- contrib/Xihe/class-00-dennett-summary.m
+++ contrib/Xihe/class-00-dennett-summary.m
@@ -1,26 +1,31 @@
 Here is my initial summary of Dennett's article.
 
-This article talks about the negative effects that
-counterfeit people bring to us, such as destroying 
-the economy, freedom of humans, culture of humans, 
-and so on. This is because democracy depends on the 
-informed consent of the governed, rather than on false 
-or embellished information (misguided). Counterfeit people
-are playing the role of misleaders here, distracting us by 
+In this article, the author talks about some negative 
+effects that counterfeit people bring to us, such as 
+destroying the economy, freedom of humans, culture of 
+humans, and so on. This is because powerful-corporations, 
+governments, and influential individuals- may control these 
+counterfeit people to  manipulate public opinion and policy, 
+endangering democracy(depends on the informed consent of the 
+governed, rather than on false or embellished information), 
+human freedom, and so on. To be more specific, counterfeit 
+people are playing the role of misleaders here, distracting us by 
 providing misleading information, leaving us confused, fearful, 
 and anxious, so that they can persuade us to adopt policies and 
 beliefs, making us more vulnerable to manipulation, and then 
-becoming passive, ignorant pawns. Furthermore, counterfeit people
-might pose a more insidious threat than nuclear weapons, as their 
-ability to autonomously reproduce could lead to a loss of control.
-Luckily, the author also pointed out that we can learn from the method 
-of identifying counterfeit banknotes to distinguish between real people
-and counterfeit people. For example, working with the manufacturers of 
-mobile phones, computers, digital TVs, and other devices to install software 
-that can interrupt any counterfeit people, but we need to get software out and 
-put it into use quickly. At the same time, we could also come up with penalties 
-(like jail time) for inventing these counterfeit people, and rewards for discovering 
-them.
+becoming passive, ignorant pawns. Another thing that we need to pay
+attention to is that counterfeit people pose a more insidious threat
+than nuclear weapons, as their ability to autonomously reproduce 
+could lead them to a loss of control. The author stresses that it 
+is important to address these negative effects before it spirals out.
+The author  pointed out that we can learn from the method of identifying
+counterfeit banknotes to distinguish between real people and counterfeit
+people. For example, working with the manufacturers of mobile phones, 
+computers, digital TVs, and other devices to install software that can 
+interrupt any counterfeit people, but we need to get software out and put 
+it into use quickly. At the same time, we could also come up with penalties 
+(like jail time) for inventing these counterfeit people, and rewards for 
+discovering them.
 
 In my opinion, everything has two sides. So, the human who invented the counterfeit
 human must have done so to fulfill some need, such as helping humans to perform 
