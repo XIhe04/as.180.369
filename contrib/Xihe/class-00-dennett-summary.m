54299bc025e8851f6b779e8a87a38a5e721a72dd
XIhe04 <143445793+XIhe04@users.noreply.github.com>
2023-09-04 16:02:02 -0400

    Update class-00-dennett-summary.m

--- contrib/Xihe/class-00-dennett-summary.m
+++ contrib/Xihe/class-00-dennett-summary.m
@@ -18,23 +18,31 @@ attention to is that counterfeit people pose a more insidious threat
 than nuclear weapons, as their ability to autonomously reproduce 
 could lead them to a loss of control. The author stresses that it 
 is important to address these negative effects before it spirals out.
-The author  pointed out that we can learn from the method of identifying
+However, luckily, the author  pointed out some ways that may help us
+to settle these problems. First, we can learn from the method of identifying
 counterfeit banknotes to distinguish between real people and counterfeit
 people. For example, working with the manufacturers of mobile phones, 
 computers, digital TVs, and other devices to install software that can 
 interrupt any counterfeit people, but we need to get software out and put 
-it into use quickly. At the same time, we could also come up with penalties 
-(like jail time) for inventing these counterfeit people, and rewards for 
-discovering them.
+it into use quickly. At the same time, we could introduce new laws to 
+penalize AI companies that misuse these technologies. The author contends
+that while the economic potential of AI is immense, AI companies should 
+share the responsibility for its ethical use.
 
 In my opinion, everything has two sides. So, the human who invented the counterfeit
-human must have done so to fulfill some need, such as helping humans to perform 
-repetitive tasks.In fact, after the appearance of counterfeit people, human do get a lot of 
-convenience, such as repetitive and boring work are replaced by artificial intelligence. 
-Counterfeit people can also help human organize information to help human save a lot of time. 
-However, if they are not used well or used by human with bad intentions, they will provide
-negative impacts, as mentioned in the article, they can destroy our civilization. 
-So I think the emergence of dummies as a technology, whether it is good or bad depends 
-on what human use it for, because dummies are just a tool, and there is no good or 
-bad in the tool itself.
+human must have done so to fulfill some need, such as helping humans perform 
+repetitive tasks. In fact, after the appearance of counterfeit people, humans do 
+get a lot of conveniences, such as repetitive and boring work being replaced by 
+artificial intelligence. Counterfeit people can also help humans organize information
+to help humans save a lot of time. However, if they are not used well or used by 
+humans with bad intentions, they will have negative impacts, as mentioned in the
+article, they can destroy our civilization. So I think the emergence of dummies 
+as a technology, whether it is good or bad depends on what humans use them for, 
+because dummies are just a tool, and there is no good or bad in the tool itself. 
+Moreover, this article made me realize clearly the importance of laws and a sense 
+of ethics. To be more specific, two ways to ensure that new things meet good needs 
+while reducing the risks they pose are new laws and a company's own sense of ethics.
+With these two in place, when companies are faced with profitable but undesirable demands, 
+companies will not act due to regulatory laws, and they are not willingness to act due
+to their ethical constraints.
 
