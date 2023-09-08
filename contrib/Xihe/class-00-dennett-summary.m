a5dfb294b93b59a52448aaf1abc272bbe127a746
XIhe04 <143445793+XIhe04@users.noreply.github.com>
2023-09-08 19:30:28 -0400

    Update class-00-dennett-summary.m

--- contrib/Xihe/class-00-dennett-summary.m
+++ contrib/Xihe/class-00-dennett-summary.m
@@ -2,47 +2,51 @@ Here is my initial summary of Dennett's article.
 
 In this article, the author talks about some negative 
 effects that counterfeit people bring to us, such as 
-destroying the economy, freedom of humans, culture of 
+destroying the economy, freedom of humans, and culture of 
 humans, and so on. This is because powerful-corporations, 
 governments, and influential individuals- may control these 
 counterfeit people to  manipulate public opinion and policy, 
 endangering democracy(depends on the informed consent of the 
 governed, rather than on false or embellished information), 
 human freedom, and so on. To be more specific, counterfeit 
-people are playing the role of misleaders here, distracting us by 
-providing misleading information, leaving us confused, fearful, 
-and anxious, so that they can persuade us to adopt policies and 
-beliefs, making us more vulnerable to manipulation, and then 
-becoming passive, ignorant pawns. Another thing that we need to pay
-attention to is that counterfeit people pose a more insidious threat
-than nuclear weapons, as their ability to autonomously reproduce 
-could lead them to a loss of control. The author stresses that it 
-is important to address these negative effects before it spirals out.
+people act as misleaders, distracting us with misleading information,
+leaving us confused, fearful, and anxious, so that they can persuade 
+us to adopt policies and beliefs, rendering us more susceptible to 
+manipulation, and then becoming passive, ignorant pawns. Another thing
+that we need to pay attention to is that counterfeit people pose a more 
+insidious threat than nuclear weapons, as their ability to autonomously 
+reproduce could lead them to a loss of control. The author stresses that it 
+is important to address these negative effects before they spiral out.
 However, luckily, the author  pointed out some ways that may help us
 to settle these problems. First, we can learn from the method of identifying
 counterfeit banknotes to distinguish between real people and counterfeit
-people. For example, working with the manufacturers of mobile phones, 
-computers, digital TVs, and other devices to install software that can 
-interrupt any counterfeit people, but we need to get software out and put 
-it into use quickly. At the same time, we could introduce new laws to 
-penalize AI companies that misuse these technologies. The author contends
-that while the economic potential of AI is immense, AI companies should 
-share the responsibility for its ethical use.
+people. For example, collaborating with manufacturers of mobile phones, computers, 
+digital TVs, and other devices to install software capable of detecting counterfeit 
+people, but we need to get software out and put it into use quickly. At the same 
+time, we could introduce new laws to penalize AI companies that misuse these 
+technologies. The author contends that while the economic potential of AI is immense, 
+AI companies should share the responsibility for its ethical use.
 
-In my opinion, everything has two sides. So, the human who invented the counterfeit
-human must have done so to fulfill some need, such as helping humans perform 
-repetitive tasks. In fact, after the appearance of counterfeit people, humans do 
-get a lot of conveniences, such as repetitive and boring work being replaced by 
-artificial intelligence. Counterfeit people can also help humans organize information
-to help humans save a lot of time. However, if they are not used well or used by 
-humans with bad intentions, they will have negative impacts, as mentioned in the
-article, they can destroy our civilization. So I think the emergence of dummies 
-as a technology, whether it is good or bad depends on what humans use them for, 
-because dummies are just a tool, and there is no good or bad in the tool itself. 
-Moreover, this article made me realize clearly the importance of laws and a sense 
-of ethics. To be more specific, two ways to ensure that new things meet good needs 
-while reducing the risks they pose are new laws and a company's own sense of ethics.
-With these two in place, when companies are faced with profitable but undesirable demands, 
-companies will not act due to regulatory laws, and they are not willingness to act due
-to their ethical constraints.
+In my opinion, the concept of "everything having two sides" applies aptly to the
+creation and deployment of artificial intelligence, including counterfeit people
+or AI-powered entities. The very invention of these counterfeit humans must have 
+stemmed from a genuine need within society. AI technology, in its various forms, 
+has revolutionized the way we live and work. For example, it has significantly 
+alleviated the burden of mundane and repetitive tasks that humans once had to 
+endure. This has translated into newfound convenience, allowing individuals to 
+focus on more creative and intellectually stimulating pursuits. Counterfeit people
+also excel in information management. They have the capability to efficiently organize 
+vast datasets, a task that could consume substantial human time and effort. In this way, 
+they become valuable tools for streamlining decision-making processes and improving 
+productivity. However, as your summary and Dennett's article have pointed out, the 
+potential misuse of AI technology is a cause for concern. When placed in the wrong hands 
+or used for nefarious purposes, counterfeit people can become instruments of deception, 
+manipulation, and harm. Therefore, I think the emergence of counterfeit people as a technology, 
+whether it is good or bad depends on what humans use them for, because counterfeit people are 
+just a tool, and there is no good or bad in the tool itself. Moreover, this article made me 
+realize clearly the importance of laws and a sense of ethics. To be more specific, two ways 
+to ensure that new things meet good needs while reducing the risks they pose are new laws and a 
+company's own sense of ethics. With these two in place, when companies are confronted with 
+lucrative yet unethical requests, companies will not act due to regulatory laws, and they choose
+not to act due to their ethical principles.
 
